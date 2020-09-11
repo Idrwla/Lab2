7b9b3d60c16eb96acc0a27e2d61d3777744dd904
Idrwla <ilyarredmi@gmail.com>
2020-09-12 01:41:45 +0600

    ready

---
+++ main.js
@@ -0,0 +1,62 @@
+function getvalue(num) {
+    var a = document.forms[0]["textview"];
+    a.value += num;
+}
+function getopcode(opt) {
+    var a = document.forms[0]["textview"];
+    if (a.value != "") {
+        if (opt == "s") {
+            a.value += "**2";
+        }
+        else if (opt == "-s") {
+        }
+        else {
+            a.value += opt;
+        }
+    }
+    else if (opt == "(" || opt == ")") {
+        a.value += opt;
+    }
+}
+function zero() {
+    var a = document.forms[0]["textview"];
+    if (a.value != "") {
+        a.value += "0";
+    }
+}
+function equal() {
+    var a = document.forms[0]["result"];
+    var temp = eval(document.forms[0]["textview"].value);
+    if (temp == "Infinity") {
+        a.value = "Hа ноль нельзя делить";
+    }
+    else {
+        a.value = temp;
+        var table = document.getElementsByTagName("table")[0];
+        if (table.rows.length >= 3) {
+            table.deleteRow(0);
+        }
+        var trow = table.insertRow(table.rows.length);
+        var cel0 = trow.insertCell(0);
+        var cel1 = trow.insertCell(1);
+        var cel2 = trow.insertCell(2);
+        cel0.innerHTML = document.forms[0]["textview"].value;
+        cel1.innerHTML = "=";
+        cel2.innerHTML = temp;
+        clean();
+    }
+    //let n  = parseInt(document.getElementById("curri").innerHTML);
+    // cel0.innerHTML =n+1;
+}
+function clean() {
+    var a = document.forms[0]["textview"];
+    a.value = '';
+    var b = document.forms[0]["result"];
+    b.value = '';
+}
+function back() {
+    var a = document.forms[0]["textview"];
+    a.value = document.forms[0]["textview"].value.substring(0, document.forms[0]["textview"].value.length - 1);
+    var b = document.forms[0]["result"];
+    b.value = '';
+}
